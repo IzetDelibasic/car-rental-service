75894b9713bd19cd3008ea102c36770eca469aea
Izet Delibasic <izetdelibasic55@gmail.com>
2023-09-23 19:12:38 +0200

    Adding a dropdown menu

--- script.js
+++ script.js
@@ -15,4 +15,18 @@ sr.reveal('.reviews-container .box',{delay: 600, origin: 'top'});
 sr.reveal('.newsletter .box',{delay: 600, origin: 'top'});
 
 
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
 
